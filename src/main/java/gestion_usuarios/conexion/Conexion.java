788ffa120ed22f68c3a195f4f3a47beb82211f27
morocho <portafoliomoro@gamil.com>
2025-12-10 11:57:21 -0500

    fix

--- src/main/java/gestion_usuarios/conexion/Conexion.java
+++ src/main/java/gestion_usuarios/conexion/Conexion.java
@@ -10,8 +10,8 @@ public class Conexion {
 
         var baseDatos = "biblioteca_db";
         var url = "jdbc:mysql://localhost:3306/" + baseDatos;
-        var usuario = "root";
-        var password = "admin";
+        var usuario = "usuario";
+        var password = "contrasena";
         try{
             Class.forName("com.mysql.cj.jdbc.Driver");
             conexion = DriverManager.getConnection(url, usuario, password);
